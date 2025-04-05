3be4fb8c7fdafd93bd054949dd5089b0225b417e
migrell <parkjiho333@naver.com>
2025-04-05 10:18:57 +0900

    로직 오타 검증

--- 250404_Processor.srcs/sources_1/new/cu.sv
+++ 250404_Processor.srcs/sources_1/new/cu.sv
@@ -1,65 +1,65 @@
-module ControlUnit (
-    input  logic clk,
-    input  logic reset,
-    output logic AsrcMuxSel,
-    output logic AEn,
-    input  logic ALt10,
-    output logic OutBuf
-);
+`timescale 1ns / 1ps
 
+module ControlUnit(
+    input logic clk,
+    input logic reset,
+    output logic ASrcMuxSel,
+    output logic AEn,
+    input logic ALt10,
+    output logic OutBuf
+    );
 
     localparam S0 = 0, S1 = 1, S2 = 2, S3 = 3, S4 = 4;
-    logic [2:0] state, state_next;
+    reg [2:0] state, next;
 
     always_ff @(posedge clk, posedge reset) begin
-        if (reset) state <= S0;
-        else state <= state_next;
+        if(reset) begin
+            state <= S0;
+        end else begin
+            state <= next;
+        end
     end
 
-
     always_comb begin
-        state_next = state;
-        AsrcMuxSel = 0;
-        AEn = 1;
+        next = state;
+        ASrcMuxSel = 0;
+        AEn = 0;
         OutBuf = 0;
         case (state)
             S0: begin
-                AsrcMuxSel = 0;
-                AEn = 1;
-                OutBuf = 0;
-                state_next = S1;
-            end
-
+               ASrcMuxSel = 0;
+               AEn = 1;
+               OutBuf = 0;
+               next = S1; 
+            end 
             S1: begin
-                AsrcMuxSel = 0;
-                AEn = 0;
-                OutBuf = 0;
-                if (ALt10) state_next = S2;
-                else state_next = S4;
+               ASrcMuxSel = 0;
+               AEn = 0;
+               OutBuf = 0;
+               if(ALt10) next = S2;
+               else next = S4;
             end
-
-
             S2: begin
-                AsrcMuxSel = 0;
-                AEn = 0;
-                OutBuf = 1;
-                state_next = S3;
+               ASrcMuxSel = 0;
+               AEn = 0;
+               OutBuf = 1;
+               next = S3; 
             end
-
             S3: begin
-                AsrcMuxSel = 0;
-                AEn = 0;
-                OutBuf = 0;
-                state_next = S4;
+               ASrcMuxSel = 1;
+               AEn = 1;
+               OutBuf = 0;
+               next = S1; 
             end
-
+            S4: begin
+               ASrcMuxSel = 0;
+               AEn = 0;
+               OutBuf = 0;
+               next = S4;
+            end
         endcase
     end
-
-
-
-
+    
 endmodule
-
 
 
